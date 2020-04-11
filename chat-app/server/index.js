2b98638467e8b03ac7264f6a9f7b61b9489ce770
apatil88 <amrut.patil88@hotmail.com>
2020-04-11 11:29:47 -0400

    Adding Join and Chat components. Adding Link routes. Adding styling to Join Component

--- chat-app/server/index.js
+++ chat-app/server/index.js
@@ -3,11 +3,21 @@ const socketio = require("socket.io");
 const http = require("http");
 
 const PORT = process.env.PORT || 5000;
+const router = require("./router");
 
 const app = express();
 const server = http.createServer(app);
 
 const io = socketio(server);
+
+io.on("connect", (socket) => {
+  console.log("A new user has connected.");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+app.use(router);
 
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
